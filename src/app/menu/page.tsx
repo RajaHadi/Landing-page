4fae3847ff9c2df89072534cd3a72e81db2de73d
RajaHadi <shabanajawed411@gmail.com>
2024-11-24 21:49:14 -0800

    @Font

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -6,7 +6,7 @@ import Zinger from '../../../public/Zinger.jpg'
 const Menu = () => {
   return (
     < div className='w-full bg-[#080C15]'>
-        <h1 className='text-center text-[#CD292A] text-3xl font-serif md:text-3xl lg:text-5xl font-extrabold p-2 pb-10'>Our Burgers</h1>
+        <h1 className='text-center text-[#CD292A] text-4xl font-black font-serif md:text-3xl lg:text-5xl  p-2 pb-10'>Our Burgers</h1>
    <div className='flex flex-col pb-10 justify-around md:flex-row sm:flex-row gap-8 pt-4  bg-[#080C15] w-full h-full'>
 <Card image={Burger} type="Anda Burger" price="150pkr" />
 <Card image={Chicken} type="Chicken Burger" price="250pkr"/>
