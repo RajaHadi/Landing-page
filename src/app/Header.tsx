682d9bfa2cf35675154c50b83f8bba2ca52d97d2
RajaHadi <shabanajawed411@gmail.com>
2024-11-28 11:12:04 -0800

    @nop

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const Header = () => {
   return (
     <div className=''>
-       <nav className="bg-[#080C15] flex justify-center items-center flex-col md:flex-row pt-2 md:justify-around  text-[#cd292a]">
+       <nav className="bg-[#080C15] pb-7 md:pb-0 flex justify-center items-center flex-col md:flex-row pt-2 md:justify-around  text-[#cd292a]">
        <Image  src={"/Burger Logo.png"} alt='Burger' height={100} width={100}/>
         <h1 className="text-center text-3xl md:text-4xl lg:text-5xl font-extrabold font-sans pt-2">
           RAJAs BURGER POINT
